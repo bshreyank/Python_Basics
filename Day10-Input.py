080a46d7a8dea204c71373e96eb182d20f992c4f
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-04-11 18:36:54 +0530

    new

---
+++ Day10-Input.py
@@ -0,0 +1,16 @@
+a=input("Enter your Name: ")
+print("My name is : ",a)
+
+'''
+Rule : the input will be String so if you try to
+        put number it will just concatenate rather 
+        than any arithematic operation.
+''' 
+x=input("Enter first number: ")
+y=input("Enter second number: ")
+print(x+y)
+#So if you want it to do arthimatic operation, do type casting
+print(int(x)+int(y))
+
+#====================================>
+
