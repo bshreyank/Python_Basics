a65e20ac251457739096280d559e4d0cc8166149
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-04-19 19:18:52 +0530

    new

---
+++ Day11-Strings.py
@@ -0,0 +1,20 @@
+#Eg 1
+name = "Shreyank"
+print("Hello, " + name)
+
+
+#Eg 2
+print('He said, "I want to eat an apple".')
+
+
+#Eg 3 - Multiline Strings
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+
+print(a)
+
+#Eg 4
+print(name[0])
+print(name[1])
