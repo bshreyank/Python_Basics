f91ed5c8c6d7aff5b3b679594ecd7cb142eacb58
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-04-25 21:48:34 +0530

    new

--- Day15-Exercise2.py
+++ Day15-Exercise2.py
@@ -1,3 +1,5 @@
+#Create a python program capable of greeting you with Good Morning, Good Afternoon and Good Evening. 
+# Your program should use time module to get the current hour. Here is a sample program and documentation link for you:
 import time
 timestamp = time.strftime('%H:%M:%S')
 print(timestamp)
@@ -8,3 +10,15 @@ print(timestamp)
 timestamp = time.strftime('%S')
 print(timestamp)
 # https://docs.python.org/3/library/time.html#time.strftime
+
+# Exercise 2
+current_time = time.localtime().tm_hour
+
+if current_time < 12:
+    greeting="Good Morning"
+elif current_time <18:
+    greeting="Good Afternoon"
+else:
+    greeting="Good Evening"
+
+print(greeting)
