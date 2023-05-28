f32428003a96659c4bc79564854b4aa7b0768ed1
SHREYANK <66244570+bshreyank@users.noreply.github.com>
2023-05-28 14:55:54 +0530

    Math-Operation

---
+++ Udemy/D2-DataType_StringManipulation/Mathematical_Operations.py
@@ -0,0 +1,30 @@
+'''
+3 + 5
+7 - 4
+3 * 2
+6 / 3
+2 ** 3
+
+######################
+
+PEMDAS LR
+
+Parentheses ()
+Exponents **
+Multiplication *
+Division / 
+Addition +
+Subtraction -
+ 
+
+* /     Multiplication and division has same priority 
+        which ever comes first from 
+        LEFT to RIGHT
+        that thing will be executed first.
+
++ -     Addition and Subtraction has same priority 
+        which ever comes first from 
+        LEFT to RIGHT
+        that thing will be executed first.
+'''
+print(3*3+3/3-3) 
